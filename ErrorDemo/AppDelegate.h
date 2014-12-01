43e26db60f740bc78c392bcf46b883d9c421bf40
yinanfang <yinan_fang@hotmail.com>
2014-11-30 22:27:42 -0500

    Initial Commit

---
+++ ErrorDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ErrorDemo
+//
+//  Created by Golden Compass on 11/30/14.
+//  Copyright (c) 2014 Golden Compass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
